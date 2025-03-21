29b5cd9eb9cf8d496bb8e3cb55638848b6a6f947
zhubaiming <other.zhubaiming@gmail.com>
2025-03-21 13:45:06 +0800

    添加通用插件 - 装载雷达插件

---
+++ src/Plugin/AddRadarPlugin.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zhubaiming\Cornerstone\Plugin;
+
+use Zhubaiming\Cornerstone\Contract\PluginInterface;
+use Zhubaiming\Cornerstone\Patchwerk;
+
+/**
+ * 装载雷达
+ *
+ * 作用: 向 Patchwerk 中设置雷达(即: 请求类)，以及雷达的各项请求属性，以方便后续直接调用请求
+ */
+class AddRadarPlugin implements PluginInterface
+{
+    public function handle(Patchwerk $patchwerk, \Closure $next): Patchwerk
+    {
+        // 1、获取请求参数
+        $parameters = $patchwerk->getParameters();
+
+        // 2、获取请求荷载
+        $payload = $patchwerk->getPayload();
+
+        // 3、设置雷达
+        $patchwerk->setRadar(new Request(xxx));
+
+
+        return $next($patchwerk);
+    }
+}
